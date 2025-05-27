9af0cc012037c9092eca0ebdf8c6ad1681bd0924
Gilber Fandino <gfandino3@gmail.com>
2025-05-27 17:47:54 -0500

    boilerplate implementada, hasta la carga de las 2 vistas

---
+++ resources/views/load_file.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Tables - SB Admin</title>
+  <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+  <link href="{{ asset('template') }}/css/styles.css" rel="stylesheet" />
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+  @include('snippets.menu_superior')
+
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+
+      @include('snippets.menu_izquierdo')
+
+    </div>
+    <div id="layoutSidenav_content">
+      <main>
+        <div class="container-fluid px-4">
+          <h3 class="mt-4">Carga de  archivo</h3>
+          <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+            <li class="breadcrumb-item active">Tables</li>
+          </ol>
+          <div class="card mb-4">
+            <div class="card-body">
+              DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
+              <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
+              .
+            </div>
+          </div>
+          <div class="card mb-4">
+            <div class="card-header">
+              <i class="fas fa-table me-1"></i>
+              DataTable Example
+            </div>
+            <div class="card-body">
+             
+            </div>
+          </div>
+        </div>
+      </main>
+      @include('snippets.footer')
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="{{ asset('template') }}/js/scripts.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+  <script src="{{ asset('template') }}/js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
---
+++ resources/views/snippets/menu_izquierdo.blade.php
@@ -0,0 +1,21 @@
+<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+  <div class="sb-sidenav-menu">
+    <div class="nav">
+      <div class="sb-sidenav-menu-heading">Core</div>
+      <a class="nav-link" href="/cargar">
+        <div class="sb-nav-link-icon"><i class="fa-solid fa-file-csv"></i></div>
+        Cargar registros
+      </a>
+
+      <a class="nav-link" href="/listar">
+        <div class="sb-nav-link-icon"><i class="fa-solid fa-table"></i></div>
+        Listar registros
+      </a>
+      
+    </div>
+  </div>
+  <div class="sb-sidenav-footer">
+    <div class="small">Logged in as:</div>
+    Start Bootstrap
+  </div>
+</nav>
